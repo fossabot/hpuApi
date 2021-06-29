2bfc682e8b221f0c03cd1ac3af39becb50647f74
Hyun <hyunsssssssss@gmail.com>
2021-06-29 13:53:43 +0800

    修复非302地址出现的timeout

--- modules/tools/redirect.js
+++ modules/tools/redirect.js
@@ -15,6 +15,7 @@ module.exports = async function(req) {
                 'User-Agent': 'Mozilla/5.0 (Linux; Android 9; PDBM00 Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045513 Mobile Safari/537.36 MMWEBID/8882 MicroMessenger/8.0.1.1841(0x2800015B) Process/tools WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64'
             }
         });
+        throw new Error('不支持的地址解析');
     } catch (error) {
         if(!error.response) return { body: {code: -1002, msg: error.message} };
         return { body: {code: 0, url: error.response.headers.location} };
